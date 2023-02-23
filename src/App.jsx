149ceda75ce83c6236ae337f8fd5aadc9052d281
Abdullah Zuberi <111603644+Abdullah-Zuberi@users.noreply.github.com>
2023-02-23 17:02:23 +0500

    Login form

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,43 @@ import React, { useState } from "react";
 const App = () => {
 
   const [name, setName] = useState(" ");
+  const [lastname, setLastName]= useState("");
   const [fullName, setFullName] = useState();
+  const [LastNamenew, setLastNameNew] = useState("");
+
+  const onSubmits=(event) =>{
+    event.preventDefault();
+    setFullName(name);
+    setLastNameNew(lastname);
+  }
 
   const inputEvent=(event) => {
     console.log( event.target.value );   
     setName(event.target.value)
   };
 
-  const onSubmit=() =>{
-    setFullName(name);
+  const inputEventTwo = (event) => {
+    setLastName(event.target.value);
   }
+
   return (
     <>
+    <div className="main_div">
+    <form onSubmits={onSubmits}>
       <div>
-        <h1>Hello{fullName}</h1>
+        <h1>Hello{fullName} {LastNamenew}</h1>
         <input type="text" placeholder="Enter Your Name"  
         onChange={inputEvent}
-        value={name}
+        // value={name}
         />
-        <button onClick={onSubmit}>Click Me 👍</button>
+        <br/>
+        <input type="text" placeholder="Enter Your Last Name"  
+        onChange={inputEventTwo}
+        value={lastname}
+        />
+        <button type='submit' onClick={onSubmits}> Submit Me👍 </button>
+      </div>
+      </form>
       </div>
     </>
   );
